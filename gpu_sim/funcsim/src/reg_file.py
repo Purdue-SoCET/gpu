9e313e162329d4280c3af884feba82a122fc74cc
liu3422 <liu3422@purdue.edu>
2025-10-18 22:37:29 -0400

    regfile fixes

--- gpu_sim/funcsim/src/reg_file.py
+++ gpu_sim/funcsim/src/reg_file.py
@@ -1,10 +1,10 @@
 from bitstring import Bits 
 class Reg_File:
     def __init__(self, size=64) -> None:
-        self.arr: list[int | float] = [0 for i in range(size)]
+        self.arr: list[Bits] = [Bits(int=0, length=32) for i in range(size)]
     
-    def read(self, idx: int) -> int | float: #potential problem: if you read from a register initially (rdat=0), it wouldn't be a "Bits" type 
-        return Bits(int=(self.arr[idx]), length=32)
+    def read(self, idx: int) -> Bits:
+        return self.arr[idx]
 
     def write(self, idx: int, val: int | float) -> None:
         self.arr[idx] = Bits(int=val, length=32)
