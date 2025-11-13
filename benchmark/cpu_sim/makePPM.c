c61a627efb599c1d0ddfabe4fa8967c0d3b046f4
mcdonou5 <mcdonou5@purdue.edu>
2025-11-13 13:50:30 -0500

    Benchmark: added float to int for makePPM

--- benchmark/cpu_sim/makePPM.c
+++ benchmark/cpu_sim/makePPM.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-void createPPMFile(char* fileName, int (*pixels)){
+void createPPMFile(char* fileName, float (*pixels)){
     FILE* file = fopen(fileName, "w");
 
     if (!file) {
@@ -19,9 +19,9 @@ void createPPMFile(char* fileName, int (*pixels)){
     for(int i = 0; i < 480; i++){     // Top to Bottom
         for(int j = 0; j < 640; j++){ // Left to Right
             int idx = 1920 * i + 3 * j;
-                sprintf(R, "%d", pixels[idx + 0]);
-                sprintf(G, "%d", pixels[idx + 1]);
-                sprintf(B, "%d", pixels[idx + 2]);
+                sprintf(R, "%d", (int)(pixels[idx + 0] * 255));
+                sprintf(G, "%d", (int)(pixels[idx + 1] * 255));
+                sprintf(B, "%d", (int)(pixels[idx + 2] * 255));
                 fputs(R, file);
                 fputs(" ", file);
                 fputs(G, file);
@@ -33,29 +33,29 @@ void createPPMFile(char* fileName, int (*pixels)){
     fclose(file);
 }
 
-/*
+
 int main()
 {
-    int (*pixel) = malloc(480 * 640 * 3 * sizeof(int));
+    float (*pixel) = malloc(480 * 640 * 3 * sizeof(float));
 
     for(int i = 0; i < 480; i++){     // Top to Bottom
         for(int j = 0; j < 640; j++){ // Left to Right
             int idx = 1920 * i + 3 * j;
                     //R G B
                 if(j < 213){
-                    pixel[idx + 0] = 255;
+                    pixel[idx + 0] = 1.0;
                     pixel[idx + 1] = 0;
                     pixel[idx + 2] = 0;
                 }
                 else if (j < 426){
                     pixel[idx + 0] = 0;
-                    pixel[idx + 1] = 255;
+                    pixel[idx + 1] = 1.0;
                     pixel[idx + 2] = 0;
                 }
                 else{
                     pixel[idx + 0] = 0;
                     pixel[idx + 1] = 0;
-                    pixel[idx + 2] = 255;
+                    pixel[idx + 2] = 1.0;
                 }
         }
     }
@@ -66,4 +66,3 @@ int main()
 
     return 0;
 }
-*/
