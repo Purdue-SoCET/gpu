ec7159c82bdeb3c510acb236141c370e813febb1
yoshilarkins <yoshilarkins@gmail.com>
2025-12-02 11:07:32 -0500

    merging ld_st unit into seth

--- gpu_sim/cyclesim/src/mem/ld_st.py
+++ gpu_sim/cyclesim/src/mem/ld_st.py
@@ -1,161 +1,193 @@
+import enum
 from typing import Optional
-from gpu_sim.cyclesim.src.mem import dcache
+# from gpu_sim.cyclesim.test import ldst
+from src.mem import dcache
+from src.mem.ld_st_payload import Ldst_Dcache_Payload, Dcache_Ldst_Payload
 from latch_forward_stage import Instruction, ForwardingIF, LatchIF
+import logging
+from bitstring import Bits
+
+logger = logging.getLogger(__name__)
 
 
 class Ldst_Fu:
     def __init__(self, mshr_size=4, ldst_q_size=4):
-
-        self.mshr: list[Coalesce] = [None for i in range(mshr_size)]
-        self.mshr_size = mshr_size
-
         self.ldst_q: list[Coalesce] = []
         self.ldst_q_size: int = ldst_q_size
 
-        self.dcache_if: Optional[LatchIF] = None
-        self.issue_if: Optional[LatchIF] = None
-        self.wb_if: Optional[ForwardingIF] = None
+        # self.dcache_if: Optional[LatchIF] = None
+        # self.issue_if: Optional[LatchIF] = None
+        # self.wb_if: Optional[LatchIF] = None
+        # self.sched_if: Optional[ForwardingIF] = None
 
         self.wb_buffer = [] #completed dcache access buffer
 
         self.outstanding = False #Outstanding dcache request addr and uid
 
-<<<<<<< HEAD
-    def connect_interfaces(self, dcache_if: "StageInterface", issue_if: "StageInterface", wb_if: "StageInterface"):
-=======
-    def connect_interfaces(self, dcache_if: LatchIF, issue_if: LatchIF, wb_if: ForwardingIF):
->>>>>>> 53dd9cab02d41caf35c2c5c2540a51116b6f48e5
-        self.dcache_if = dcache_if
-        self.issue_if = issue_if
-        self.wb_if = wb_if
+    def connect_interfaces(self, dcache_if: LatchIF, issue_if: LatchIF, wb_if: LatchIF, sched_if: ForwardingIF):
+        self.dcache_if: LatchIF = dcache_if
+        self.issue_if: LatchIF = issue_if
+        self.wb_if: LatchIF = wb_if
+        self.sched_if: ForwardingIF = sched_if
+    
+    def forward_miss(self, instr: Instruction):
+        self.sched_if.push(instr)
 
-<<<<<<< HEAD
-    def tick(self, instr):
-        if len(self.ldst_q) + 1 < self.ldst_q_size:
-            self.ldst_q.append(Coalesce(instr))
-        
-        if len(self.ldst_q) == self.ldst_q_size:
-            self.issue_if.push({"full": 1}) # backpressure
-        
-        if self.outstanding == False:
-            coalesce: Coalesce = self.ldst_q[0]
-            addr = coalesce.genRequestAddr()
-            mode = coalesce.write
-            uid = 0
-        else:
-            if dcache_if.hit:
-                if dcache_if.data.uid == 1:
-                    
-
-                self.outstanding = False
-        
-
-class Coalesce():
-    cache_line_mask = 0xFF_FF_FF_FF_FF_FF_FF_00
-=======
     def tick(self):
-
+        # self.ldst_q[0].logState()
         #populate ldst_q if not full
         if len(self.ldst_q) + 1 < self.ldst_q_size:
             instr = self.issue_if.pop()
             if instr != None:
                 self.ldst_q.append(Coalesce(instr))
 
-        #apply backpressure if full
+        #apply backpressure if ldst_q full
         if len(self.ldst_q) == self.ldst_q_size:
             self.issue_if.forward_if.set_wait(True)
         else:
             self.issue_if.forward_if.set_wait(False)
 
+        #send instr to wb if ready
+        if self.wb_if.ready_for_push():
+            if len(self.wb_buffer) > 0:
+                self.wb_if.push(self.wb_buffer.pop(0).instr)
+        
         #Push cache access if no outstanding
-        if self.outstanding == False and self.dcache_if.ready_for_push():
+        if self.outstanding == False and self.dcache_if.ready_for_push() and len(self.ldst_q) > 0 and not self.ldst_q[0].readyWB() and len(self.ldst_q[0].pending_addrs) > 0:
+            #push cache access if no outstanding and dcache ready and instr not complete
             coal: Coalesce = self.ldst_q[0]
             addr = coal.genRequestAddr()
-            mode = coal.write
+            write = coal.write
+            pc = coal.instr.pc
             self.outstanding = True
-
             self.dcache_if.push(
-                {
-                    "instr":coal.instr
-                    "addr": addr,
-                    "mode": mode
-                }
+                Ldst_Dcache_Payload(addr=addr, write=write, pc=pc)
             )
-        elif self.dcache_if.forward_if.payload["hit"] == True:
-            payload = self.dcache_if.forward_if.pop()
-            self.outstanding = False
-            coal: Coalesce = self.ldst_q[0]
-            coal.parseHit(payload)
 
-        elif self.dcache_if.forward_if.payload["miss"] == True:
-            payload = self.dcache_if.forward_if.pop()
-            if len(self.wb_buffer) != 0:
-                self.wb_buffer[0].parseHit(payload)
+        #Handle hit or miss
+        if self.dcache_if.forward_if.pop():
+            if len(self.ldst_q) == 0:
+                logger.warning(f"LSQ is length 0 and recieved a dcache response")
+            payload: Dcache_Ldst_Payload = self.dcache_if.forward_if.pop()
+            ldst_coal = self.ldst_q[0]
+            if payload.hit == True and ldst_coal.in_flight_addr:
+                if ldst_coal.inRange(payload.addr, ldst_coal.in_flight_addr):
+                    self.outstanding = False
+                    ldst_coal.parseHit(payload)
+                    if ldst_coal.readyWB():
+                        ldst_coal = self.ldst_q.pop(0)
+                        self.wb_buffer.append(ldst_coal)
+                else:
+                    logging.warning("Error: Cache hit wasn't a LDST_Q addr")
+            elif payload.hit and ldst_coal.inMSHR(payload.addr):
+                ldst_coal.parseHit(payload)
+                if ldst_coal.readyWB():
+                        ldst_coal = self.ldst_q.pop(0)
+                        self.wb_buffer.append(ldst_coal)
+                        self.outstanding = False
+            elif payload.miss == True:
+                if len(self.ldst_q) == 0:
+                    logger.warning("LSQ is length 0 and recieved a dcache response")
+                if not ldst_coal.missed:
+                    self.forward_miss(ldst_coal.instr)
+                ldst_coal.parseMiss(payload)
+                self.outstanding = False
             else:
-                self.ldst_q[0].parseHit(payload)
-        
+                logger.warning(f"Dcache response ignored")
+                logger.warning(f" {payload.addr}")
+                logger.warning(f" {ldst_coal.inMSHR(payload.addr)}")
+                logger.warning(f" {ldst_coal.mshr_addrs}")
+
+
 
 class Coalesce():
-    cache_line_mask = 0xFF_FF_FF_FF_FF_FF_FC_00
->>>>>>> 53dd9cab02d41caf35c2c5c2540a51116b6f48e5
-    cache_fetch_step = 0x4
+    cache_line_mask =  '0xFF_FF_FF_80'
 
     def __init__(self, instr):
-        self.instr = instr
-        self.write = True # Whether instruction is a ld or st
-        self.addrs = [] # initialized with all addr mem addrs
-        self.data = []
-        self.pending_addrs = []
-<<<<<<< HEAD
-        self.in_flight_idx = [0 for i in range(32)]
-=======
-        self.finished_idxs = [0 for i in range(32)]
-        self.mshr_addrs = []
->>>>>>> 53dd9cab02d41caf35c2c5c2540a51116b6f48e5
-        self.in_flight_addr = 0
+        self.instr: Instruction = instr
+        self.write = False # Whether instruction is a ld or st
+        self.addrs: list[Bits] = [] # initialized with all addr mem addrs
+        self.pending_addrs = [] # List of to be issued addrs
+        self.finished_idxs = [0 for i in range(32)] # Hot list of completed thread hits
+        self.mshr_addrs = [] # List of missed addresses waiting for mshr hit
+        self.in_flight_addr = None # None when no in-flight
+        self.missed = False #Flag to indicate whether 
+        for i in range(32):
+            addr = Bits(int=self.instr.rdat1[i].int + self.instr.rdat2[i].int, length=32)
+            self.addrs.append(addr)
+            if addr & self.cache_line_mask not in self.pending_addrs:
+                self.pending_addrs.append(addr & self.cache_line_mask)
+        self.instr.wdat = [None for i in range(32)]
 
-    def inRange(self, base_addr, addr):
+
+    def inRange(self, base_addr: Bits, addr: Bits) -> bool:
         '''
         Returns whether addr can be coalesced with base_addr
         '''
-        base_addr = base_addr & 0xFF_FF_FF_FF_FF_FF_FF_00
-        addr = addr & 0xFF_FF_FF_FF_FF_FF_FF_00
-        pass
+        if base_addr == None or addr == None:
+            return False
+        base_addr = base_addr & self.cache_line_mask
+        addr = addr & self.cache_line_mask
+        if addr == base_addr:
+            return True
+        else:
+            return False
+    
+    def inMSHR(self, addr: Bits) -> bool:
+        if len(self.mshr_addrs) == 0:
+            return False
+    
+        addr = addr & self.cache_line_mask
+        if addr == self.mshr_addrs[0]:
+            return True
+        return False
 
     def genRequestAddr(self) -> int:
-<<<<<<< HEAD
-        self.in_flight_addr = self.pending_addrs[0] & coalesce.cache_line_mask
-=======
-        self.in_flight_addr = self.pending_addrs[0] & Coalesce.cache_line_mask
->>>>>>> 53dd9cab02d41caf35c2c5c2540a51116b6f48e5
+        if len(self.pending_addrs) == 0:
+            self.logState(logging.WARN)
+        self.in_flight_addr = self.pending_addrs.pop(0) & self.cache_line_mask
         coal_addrs = [addr for addr in self.pending_addrs if self.inRange(self.in_flight_addr, addr)]
-        for idx, addr in self.addrs:
-            if addr in coal_addrs:
-                self.in_flight_idx[idx] = 1
         return self.in_flight_addr
 
-    def parseHit(self, data):
-<<<<<<< HEAD
-        if len(data) != 32:
-            print("Err: data is not of right size")
-            print(data)
-=======
->>>>>>> 53dd9cab02d41caf35c2c5c2540a51116b6f48e5
-        data_addr = self.in_flight_addr
-        for i, d in enumerate(data):
-            if self.in_flight_idx[i]:
-                self.data[i] = d
-<<<<<<< HEAD
-            data_addr += coalesce.cache_fetch_step
+    def parseHit(self, payload) -> None:
+        data_addr = payload.addr
+        logger.info(f"Parsing hit for addr {payload.addr}")
+
+        #Sanity check that addr is in-flight or mshr
+        if not (self.inRange(self.in_flight_addr, data_addr) or self.inMSHR(data_addr)):
+            logger.warning("Err: parseHit called for payload not in-flight or in MSHR")
+
+        if self.write == False:
+            for idx, addr in enumerate(self.addrs):
+                if self.inRange(data_addr, addr):
+                    d_idx = (addr.int - data_addr.int) >> 5
+                    self.instr.wdat[idx] = payload.data[d_idx]
+                    self.finished_idxs[idx] = 1
+                    if self.inMSHR(addr):
+                        self.mshr_addrs.pop(0)
+                    else:
+                        self.in_flight_addr = None
+        self.in_flight_addr = None
         
-        self.in_flight_idx = [0 for i in range(32)]
-    
+
+    def parseMiss(self, payload) -> None:
+        if self.in_flight_addr != payload.addr:
+            logger.warning(f"Err: in-flight-address not same as miss address")
+        self.mshr_addrs.append(self.in_flight_addr)
+        
+        self.in_flight_addr = None
+        self.missed = True
+
+    def readyMSHR(self):
+        return len(self.pending_addrs) == 0 and self.in_flight_addr == None
+
     def readyWB(self):
-        return len(self.pending_addrs) == 0 and not any(self.in_flight_idx)
-=======
-            data_addr += Coalesce.cache_fetch_step
-        self.in_flight_idx = [0 for i in range(32)]
+        return all(self.finished_idxs) and len(self.pending_addrs) == 0 and self.in_flight_addr == None
     
-    def readyWB(self):
-        return all(self.finished_idxs)
->>>>>>> 53dd9cab02d41caf35c2c5c2540a51116b6f48e5
+    def logState(self, level=logging.INFO):
+        logger.log(level, f"num pending: {len(self.pending_addrs)}")
+        logger.log(level, f"num mshr   : {len(self.mshr_addrs)}")
+        logger.log(level, f"finish idxs: {str(self.finished_idxs)}")
+        logger.log(level, f"wdat {self.instr.wdat}")
+        logger.log(level, f"wdat size {len(self.instr.wdat)}")
+        logger.log(level, f"pending    : {str(self.pending_addrs)}")
